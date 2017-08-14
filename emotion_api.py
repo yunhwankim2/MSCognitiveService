e21a1f9ba7096d81635ee479ae3c20f87e1d608e
yunhwankim2 <yunhwankim2@gmail.com>
2017-08-14 16:04:36 +0900

    add emotion_api.py

--- emotion_api.py
+++ emotion_api.py
@@ -1,17 +1,26 @@
-######## 얼굴 개수 카운트 및 드러난 감정 계산 ########
+# Microsoft Cognitive Service 를 이용한 
+# 사진당 표현된 얼굴 개수 카운트 및 
+# 얼굴당 표현된 감정 추출
+# 2017/8/14 현재 Microsoft Azure Cognitive Service 로 이름이 바뀜.
 
 import requests
-import pprint
 import os
+
+# subscription key 를 확보하여 아래에 복사해 넣어주어야 함.
+# Microsoft Azure Cognitive Service 
+# (https://azure.microsoft.com/ko-kr/services/cognitive-services/)
+# 로 접속하여 무료 평가판 메뉴를 통해 무료 key 를 발급받거나,
+# Azure 계정을 통해 유료 key 를 발급.
 
 subscription_key = ""
 
-ids_path = "./images/"
+ids_path = "./images/"      # images 폴더 내에 계정 id 별로 별도의 폴더가 만들어져 있고, 그 폴더 안에 사진이 들어 있다고 가정
 user_ids = os.listdir(ids_path)
 user_ids = [user_id for user_id in user_ids if not user_id.startswith('.')]
 
-results_path = "./results/"
+results_path = "./results/"     # 분석 결과를 담은 .json 파일이 저장될 경로
 
+# 개별 사진을 서버로 보내 분석을 실시할 함수
 def face_num_emotion(filename, subscription_key):
     # The supported input image formats includes JPEG, PNG, GIF(the first frame), BMP. 
     # Image file size should be no larger than 4MB.
@@ -34,6 +43,7 @@ def face_num_emotion(filename, subscription_key):
         results = None
     
     # If no face is detected, an empty array will be returned. 
+    
     """ sample error
         {'error': {'code': 'InvalidImageSize',
           'message': 'Image size is too small or too big.'}}
@@ -42,6 +52,7 @@ def face_num_emotion(filename, subscription_key):
     
 ######################################################################
 
+# 모든 폴더의 모든 사진을 루프를 돌면서 분석 실시
 for user_id in user_ids:
     result_name = results_path + "/" + user_id
 
