1aea87c241c543f88d4c0361f366e795331529a5
yunhwankim2 <yunhwankim2@gmail.com>
2017-08-14 16:08:20 +0900

    add computervision_api.py

--- computervision_api.py
+++ computervision_api.py
@@ -1,19 +1,25 @@
-
-########  Microsoft Computer Vision 을 이용한 사진 내용 분석  ########
+# Microsoft Cognitive Service 를 이용한 
+# 사진의 내용 분석
+# 2017/8/14 현재 Microsoft Azure Cognitive Service 로 이름이 바뀜.
 
 import requests
-import pprint
 import os
+
+# subscription key 를 확보하여 아래에 복사해 넣어주어야 함.
+# Microsoft Azure Cognitive Service 
+# (https://azure.microsoft.com/ko-kr/services/cognitive-services/)
+# 로 접속하여 무료 평가판 메뉴를 통해 무료 key 를 발급받거나,
+# Azure 계정을 통해 유료 key 를 발급.
 
 subscription_key = ""
 
-ids_path = "./images/"
+ids_path = "./images/"      # images 폴더 내에 계정 id 별로 별도의 폴더가 만들어져 있고, 그 폴더 안에 사진이 들어 있다고 가정
 user_ids = os.listdir(ids_path)
 user_ids = [user_id for user_id in user_ids if not user_id.startswith('.')]
 
-results_path = "./results/"
+results_path = "./results/"     # 분석 결과를 담은 .json 파일이 저장될 경로
 
-
+# 개별 사진을 서버로 보내 분석을 실시할 함수
 def computer_vision_api(filename, subscription_key):
     
     headers = {             # Request headers 
@@ -42,8 +48,8 @@ def computer_vision_api(filename, subscription_key):
 
 #########################
 
+# 모든 폴더의 모든 사진을 루프를 돌면서 분석 실시
 for user_id in user_ids:
-
     result_name = results_path + "/" + user_id
 
     photo_path = ids_path + user_id
@@ -53,7 +59,7 @@ for user_id in user_ids:
     with open(result_name + "_cv.jsonl", 'a') as f:
         for photo_name in photo_names:
             image_name = photo_path + '/' + photo_name
-            results = anl.computer_vision_api(image_name, cv_subscription_key)
+            results = computer_vision_api(image_name, cv_subscription_key)
             photo_dict = {}
             photo_dict[photo_name] = results
             f.write(json.dumps(photo_dict) + '\n')
